2fef4a9e2afe479aba5e7e8b78721212f7d6e7a1
Eugene Brodsky <fupslot@gmail.com>
2017-12-25 21:47:35 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { render } from "react-dom";
+import Hello from "./Hello";
+import ms from "ms";
+
+const styles = {
+  fontFamily: "sans-serif",
+  textAlign: "center"
+};
+
+const App = ({name}) => (
+  <div style={styles}>
+    <Hello name="CodeSandbox" />
+    <h2>
+      Start editing to see {name} some magic happen {"\u2728"}
+    </h2>
+  </div>
+);
+
+render(<App name="hello"/>, document.getElementById("root"));
